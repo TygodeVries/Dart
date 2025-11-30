27df82b8bbb3213121735d8edbee8b5035422328
Pruyque <unconfigured@null.spigotmc.org>
2025-11-30 21:56:07 +0100

    Take target asset folder from commandline

--- Project.Editor/EntryPoint.cs
+++ Project.Editor/EntryPoint.cs
@@ -21,7 +21,17 @@ namespace Editor
         public static void Main()
         {
             Debug.Log("Loading Editor...");
-            GuiWindow.Enable(new NavBarUI());
+   			string[] args = Environment.GetCommandLineArgs();
+
+         for (int cx = 0; cx < args.Length - 1; cx++)
+         {
+            if (args[cx] == "-p")
+            {
+               Project.Editor.Editor.projectPath = args[cx + 1];
+            }
+         }
+
+	   		GuiWindow.Enable(new NavBarUI());
             GuiWindow.Enable(new ProjectWindow());
             GuiWindow.Enable(new InspectorWindow());
 
