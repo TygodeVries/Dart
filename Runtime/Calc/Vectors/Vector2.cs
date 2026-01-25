ba590e920401fb04be775d33605f24608c2fb647
Pruyque <unconfigured@null.spigotmc.org>
2026-01-25 14:01:43 +0100

    Vector2 scalar multiplication

--- Runtime/Calc/Vectors/Vector2.cs
+++ Runtime/Calc/Vectors/Vector2.cs
@@ -66,8 +66,16 @@ namespace Runtime.Calc
         {
             return new Vector2(a.x * b.x, a.y * b.y);
         }
+		public static Vector2 operator *(Vector2 a, float b)
+		{
+			return new Vector2(a.x * b, a.y * b);
+		}
+		public static Vector2 operator *(float b, Vector2 a)
+		{
+			return new Vector2(a.x * b, a.y * b);
+		}
 
-        public static Vector2 operator /(Vector2 a, Vector2 b)
+		public static Vector2 operator /(Vector2 a, Vector2 b)
         {
             return new Vector2(a.x / b.x, a.y / b.y);
         }
